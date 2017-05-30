ec5dfcb7a5c6a4284b06c8d0bb102480527beaeb
Konrad Cybulski <ko.cybulski@gmail.com>
2017-05-30 11:58:13 +1000

    Created plots

--- Assignment3_analysis.R
+++ Assignment3_analysis.R
@@ -1,25 +1,23 @@
-library(xlsx)
 library(ggplot2)
 library(dplyr)
 library(reshape)
 library(grid)
 
-setwd("~/Uni/FIT2083/Assignment3")
+setwd("~/Uni/FIT2083/GlobalTerrorismAnalysis")
 
 # ---- TODO: ----
 # Look at successes by the number of deaths per act. To do this, 
 # group by nkill, e.g. nkill < 5, < 10, < 20, < 50, < 100, etc...
 # Can be done with a for loop and breaking if statement:
 
-sf <- read.csv2("Data/gtd/globalterrorism.csv", sep=",")
+sf <- read.csv("Data/gtd/globalterrorism.csv", sep=",")
 
 sf_known <- sf[sf$nkill != "", ]
 #sf[sf$nkill == "", ]$nkill <- mean(as.numeric(as.character(unlist(sf[sf$nkill != "", ]$nkill))))
 sf_known$nkill <- as.numeric(as.character(unlist(sf_known$nkill)))
 head(sf)
-successes <- vector(length = length(unique(sf$iyear)), mode="numeric")
-attempts <- vector(length = length(unique(sf$iyear)), mode="numeric")
-deaths <- vector(length = length(unique(sf$iyear)), mode="numeric")
+successes_group <- list(mode="vector")
+attempts_group <- list(mode="vector")
 deaths_group <- list(mode="vector")
 groups <- c(1, 2, 5, 10, 20, 30, 50, 100,150,200,250,300,400,500,1000,2000)
 for(year in unique(sf_known$iyear)){
@@ -28,47 +26,49 @@ for(year in unique(sf_known$iyear)){
     if(i == 1){
       deaths_group[[as.character(year)]][[i]] <- length(which(sf_known_year$nkill < groups[[i]]))
     }else{
-      deaths_group[[as.character(year)]][[i]] <- length(which(sf_known_year$nkill < groups[[i]] & sf_known_year$nkill >= groups[[i-1]]))# - sum(deaths_group[[as.character(year)]][1:i-1])
+      deaths_group[[as.character(year)]][[i]] <- length(which(sf_known_year$nkill < groups[[i]] & sf_known_year$nkill >= groups[[i-1]]))
     }
   }
 }
 
 death_frame <- data.frame(t(data.frame(deaths_group))[2:46, ])
 death_frame$Year <- unique(sf$iyear)
-colnames(death_frame)[1:(ncol(death_frame)-1)] <- paste(" < ", as.character(groups), sep = "")
+colnames(death_frame)[1] <- "  0 deaths"
+colnames(death_frame)[2:(ncol(death_frame)-1)] <- paste(" < ", as.character(groups[2:length(groups)]), " deaths", sep = "")
 death_frame <- melt(death_frame, id.vars = "Year", variable_name = "series")
 
 death_frame$Year <- as.numeric(death_frame$Year)
 death_frame$value <- as.numeric(as.character(unlist(death_frame$value)))
-
-p <- ggplot(death_frame, aes(x=Year, y=value, colour=series)) +
-  #geom_smooth(method='lm', alpha=0.07) +
-  #geom_point() + 
+high_death_frame <- death_frame[death_frame$series != c("  0 deaths", " < 2 deaths", " < 5 deaths"), ]
+p <- ggplot(high_death_frame, aes(x=Year, y=value, colour=series)) +
   geom_text(data = subset(death_frame, Year == max(death_frame$Year)), aes(label = series, colour = series, x = Inf, y = value), hjust = -.1) +
   geom_line() +
   #geom_point() +
-  scale_colour_discrete(guide = 'none')  +    
-  theme(plot.margin = unit(c(1,3,1,1), "lines")) 
-  #scale_y_continuous(limits = c(0, 2000))
+  scale_colour_discrete(guide = 'none')  +  
+  theme_bw() + 
+  theme(plot.margin = unit(c(1,6,1,1), "lines")) +
+  scale_y_continuous(limits = c(0,1100), expand=c(0,0)) +
+  scale_x_continuous(limits = c(1970, 2015), expand=c(0,0))+
+  labs(x="Year", y="# of Attacks")
+#scale_y_continuous(limits = c(0, 2000))
 gt <- ggplotGrob(p)
 gt$layout$clip[gt$layout$name == "panel"] <- "off"
 grid.draw(gt)
 
- # ---- Successes vs Attempts
+# ---- Successes vs Attempts
 count <- 1
 for(year in unique(sf$iyear)){
   successes[count] <- sum(sf[sf$iyear == year, ]$success)
   attempts[count] <- nrow(sf[sf$iyear == year, ])
-  deaths[count] <- sum(sf[sf$iyear == year, ]$nkill)
   count <- count + 1
 }
 success_time <- data.frame(unique(sf$iyear), successes, attempts, deaths)
 colnames(success_time) <- c("Year", "Successes", "Attempts", "Deaths")
 success_time$Proportion <- success_time$Successes/success_time$Attempts
 
-ggplot(success_time, aes(x=Year, y=Deaths)) + 
+ggplot(success_time, aes(x=Year, y=Proportion)) + 
+  labs(x="Year", y="Successes/Attempts") +
   geom_point() + 
   geom_line() + 
-  theme_bw()
-  #scale_y_continuous(limits=c(0,1))
-cor(sf)
+  theme_bw()+
+  scale_y_continuous(limits=c(0,1))
