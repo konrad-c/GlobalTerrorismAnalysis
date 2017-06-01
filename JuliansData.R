b6fcd1f992d454aa7a3a72cdc87f9dd88b6569b3
Konrad Cybulski <ko.cybulski@gmail.com>
2017-06-01 16:07:48 +1000

    Created plots of News data compared to terrorism data

--- JuliansData.R
+++ JuliansData.R
@@ -1,28 +1,39 @@
-library(xlsx)
 library(ggplot2)
 library(dplyr)
 library(reshape)
 library(gridExtra)
+library(lubridate)
 
-setwd("/UNI/Y2S1/2083/Assignment3/globalTerrorismAnalysis")
+#setwd("/UNI/Y2S1/2083/Assignment3/globalTerrorismAnalysis")
 
 # --Task---
 #  
 
 # read the data
 sf <- read.csv2("Data/gtd/globalterrorism.csv", sep=",")
+# Clean bad data
+sf <- sf[sf$imonth != "0", ]
 
 # graoh the number of deads in the last 45 years i neach of the countries
 sub_countries <- sf$country_txt
 sub_years <- sf$iyear
+sub_months <- sf$imonth
 sub_deaths <- sf$nkill
+sub_attacktype <- sf$attacktype1_txt
 # Casting of variables
 sub_countries <- as.character(sub_countries)
 sub_deaths <- as.numeric(as.character(unlist(sub_deaths)))
 sub_years <- as.numeric(as.character(unlist(sub_years)))
+sub_months <- as.character(unlist(sub_months))
+sub_attacktype <- as.character(unlist(sub_attacktype))
+sub_months[as.numeric(sub_months) < 10] <- paste("0", sub_months[as.numeric(sub_months) < 10], sep="")
+
+# Create Date columns:
+sub_date <- paste("01", sub_months, paste0(sub_years," 00:00:00"), sep="/")
+sub_date <- as.Date(as.character(as.POSIXct(strptime(sub_date, format="%d/%m/%Y %H:%M:%S"))))
 
 # Create reduced data.frame
-reduced_sf <- data.frame(sub_years, sub_countries, sub_deaths)
+reduced_sf <- data.frame(sub_years, sub_date, sub_countries, sub_deaths, sub_attacktype)
 reduced_sf <- na.omit(reduced_sf)
 
 # Create data from reduced set for unique list of countries
@@ -35,7 +46,7 @@ for(country in countries){
 }
 deaths_country <- data.frame(countries, deaths) 
 deaths_country <- deaths_country[order(deaths_country$deaths, decreasing = T), ]
-n <- 15
+n <- 6
 plot_data <- deaths_country[1:n, ]
 plot_data$countries <- factor(plot_data$countries, levels = as.character(plot_data$countries))
 
@@ -48,7 +59,7 @@ ggplot(plot_data, aes(x=countries, weight=deaths)) +
   scale_y_continuous(limits = c(0,63000), expand = c(0.01,0))
 
 
-# ---- Country specific deaths over time:
+# ---- Country specific deaths over YEARS:
 countries <- as.character(unique(sf$country_txt))
 years <- unique(sf$iyear)
 count <- 1
@@ -99,16 +110,80 @@ top_western_countries <- deaths_country[grep(paste(western_countries, collapse='
 top_western_countries <- top_western_countries[order(top_western_countries$deaths),]
 top_western_countries <- as.character(top_western_countries$countries)
 deaths_time <- deaths_country_time[grep(paste(top_western_countries, collapse='|'), deaths_country_time$Country, ignore.case=TRUE),]
-deaths_western_plot <- ggplot(deaths_time, aes(x=Year, y=Deaths, fill=Country, colour=Country)) +
-  geom_line() #+
-  #scale_y_continuous(limits=c(0,500)) 
-attacks_western_plot <- ggplot(deaths_time, aes(x=Year, y=Attacks, fill=Country, colour=Country)) +
-  geom_line() +
-  scale_y_continuous(limits=c(0,500))
-deaths_per_attack_western <- ggplot(deaths_time, aes(x=Year, y=Deaths/Attacks, fill=Country, colour=Country)) +
+deaths_western_plot <- ggplot(deaths_time, aes(x=Year, y=Deaths, colour=Country)) +
   geom_line() 
-grid.arrange(deaths_western_plot, attacks_western_plot, ncol=2)
+attacks_western_plot <- ggplot(deaths_time, aes(x=Year, y=Attacks, colour=Country)) +
+  geom_line()
+grid.arrange(deaths_western_plot, attacks_western_plot, ncol=1)
+# ---- Plot All Deaths and Attacks in all countries:
+deaths_vec <- vector(mode="numeric")
+attacks_vec <- vector(mode="numeric")
+year_vec <- vector(mode="numeric")
+count <- 1
+for(year in unique(deaths_time$Year)){
+  year_vec[[count]] <- year
+  deaths_vec[[count]] <- sum(deaths_time[deaths_time$Year == year, ]$Deaths)
+  attacks_vec[[count]] <- sum(deaths_time[deaths_time$Year == year, ]$Attacks)
+  count <- count + 1
+}
+deaths_time_total <- data.frame(
+  Year=year_vec,
+  Deaths=deaths_vec,
+  Attacks=attacks_vec
+)
+deaths_western_total_plot <- ggplot(deaths_time_total, aes(x=Year, y=Deaths, colour="red")) +
+  geom_line() +
+  scale_y_continuous(limits=c(0,700))
+attacks_western_total_plot <- ggplot(deaths_time_total, aes(x=Year, y=Attacks, colour="blue")) +
+  geom_line() +
+  scale_y_continuous(limits=c(0,700))
+grid.arrange(deaths_western_total_plot, attacks_western_total_plot, ncol=1)
 
 
 
+# ---- Country specific deaths over MONTHS:
+countries <- unique(reduced_sf$sub_countries)
+dates <- unique(reduced_sf$sub_date)
+count <- 1
+country_vec <- vector(mode="character")
+dates_vec <- vector(mode="numeric")
+year_vec <- vector(mode="numeric")
+month_vec <- vector(mode="numeric")
+death_vec <- vector(mode="numeric")
+attack_vec <- vector(mode="numeric")
+for(country in countries){
+  reduced_sf_set <- reduced_sf[reduced_sf$sub_countries == country, ]
+  for(i in 1:length(dates)){
+    date <- dates[[i]]
+    country_vec[[count]] <- country
+    dates_vec[[count]] <- date
+    death_set <- subset(reduced_sf_set, sub_date == date)
+    death_vec[[count]] <- round(sum(death_set$sub_deaths))
+    attack_vec[[count]] <- nrow(death_set)
+    year_vec[count] <- year(date)
+    month_vec[count] <- month(date)
+    count <- count+1
+  }
+}
+deaths_country_time <- data.frame(
+  Country=country_vec,
+  Date=dates_vec,
+  Year=year_vec,
+  Month=month_vec,
+  Deaths=death_vec,
+  Attacks=attack_vec
+)
+
+top_n_countries <- plot_data$countries
+graph_vec <- list()
+for(country in top_n_countries){
+  deaths_time <- deaths_country_time[deaths_country_time$Country == country, ]
+  graph_vec[[country]] <- ggplot(data=deaths_time, aes(x=Month, y=Deaths, colour=factor(Year))) +
+    geom_line() +
+    labs(x="Date", y="Deaths", title=country) +
+    theme(legend.title = element_blank())
+}
+grid.arrange(grobs = graph_vec, ncol=ceiling(sqrt(n)))
+
+# ---- Looking at attack type over time:
 
---
+++ NewsDataAnalysis.R
@@ -0,0 +1,100 @@
+library(ggplot2)
+library(dplyr)
+library(reshape)
+library(gridExtra)
+library(lubridate)
+install.packages(devtools)
+devtools::install_github("baptiste/egg")
+library(egg)
+
+# --Task---
+#  
+sf <- read.csv2("Data/gtd/globalterrorism.csv", sep=",")
+
+# read the data
+articles_sf <- read.csv2("Crawler/articleCount.csv", sep=",")
+articles_sf$ArticleType <- "Total"
+terror_articles_sf <- read.csv2("Crawler/TerrorArticleCount.csv", sep=",")
+terror_articles_sf$ArticleType <- "Terror Article"
+sf_news <- rbind(articles_sf, terror_articles_sf)
+sf_news$Year <- as.numeric(sf_news$Year)
+sf_news$NumArticles <- as.numeric(sf_news$NumArticles)
+# ---- Plot Data ----
+numarticle <- ggplot(sf_news, aes(x=Year, y=NumArticles, colour=ArticleType)) +
+  geom_point() +
+  geom_line() + 
+  theme_bw() +
+  scale_color_discrete(name="Article Type") +
+  labs(x="Year", y="# of Articles")
+sf_prop <- data.frame(
+  Year=articles_sf$Year, 
+  Proportion=terror_articles_sf$NumArticles/articles_sf$NumArticles
+)
+proparticle <- ggplot(sf_prop, aes(x=Year, y=Proportion)) +
+  geom_point() +
+  geom_line() + 
+  theme_bw() +
+  labs(x="Year", y="Ratio of Terrorism Articles to Total Articles") +
+  geom_smooth(method="lm", alpha=0.0)
+grid.arrange(numarticle, proparticle, ncol=1)
+
+# ---- Terror Articles vs Attacks & Deaths
+# Clean bad data
+sf <- sf[sf$imonth != "0", ]
+
+# graoh the number of deads in the last 45 years i neach of the countries
+sub_countries <- sf$country_txt
+sub_years <- sf$iyear
+sub_months <- sf$imonth
+sub_deaths <- sf$nkill
+sub_attacktype <- sf$attacktype1_txt
+# Casting of variables
+sub_countries <- as.character(sub_countries)
+sub_deaths <- as.numeric(as.character(unlist(sub_deaths)))
+sub_years <- as.numeric(as.character(unlist(sub_years)))
+sub_months <- as.character(unlist(sub_months))
+sub_attacktype <- as.character(unlist(sub_attacktype))
+sub_months[as.numeric(sub_months) < 10] <- paste("0", sub_months[as.numeric(sub_months) < 10], sep="")
+
+# Create Date columns:
+sub_date <- paste("01", sub_months, paste0(sub_years," 00:00:00"), sep="/")
+sub_date <- as.Date(as.character(as.POSIXct(strptime(sub_date, format="%d/%m/%Y %H:%M:%S"))))
+
+# Create reduced data.frame
+reduced_sf <- data.frame(sub_years, sub_date, sub_countries, sub_deaths, sub_attacktype)
+reduced_sf <- reduced_sf[reduced_sf$sub_countries == "United States", ]
+reduced_sf <- na.omit(reduced_sf)
+
+count <- 1
+year_vec <- vector(mode="numeric")
+death_vec <- vector(mode="numeric")
+attack_vec <- vector(mode="numeric")
+for(year in years){
+  year_vec[[count]] <- year
+  death_set <- subset(reduced_sf, sub_years == year)
+  death_vec[[count]] <- round(sum(death_set$sub_deaths))
+  attack_vec[[count]] <- length(which(sf_set$iyear == year))
+  count <- count+1
+}
+us_sf <- data.frame(
+  Year=year_vec,
+  Deaths=death_vec,
+  Attacks=attack_vec
+  #TerrorArticles=sf_news[sf_news$ArticleType=="Terror Article", ]$NumArticles[1:45]
+)
+us_sf <- melt(data = us_sf, id.vars = "Year")
+us_plot <- ggplot(us_sf, aes(x=Year,y=value, colour=variable)) +
+  geom_line() +
+  scale_colour_discrete(name="") +
+  theme(legend.position = "left") +
+  labs(x="", y="Number of", title="United States Terror & News Statistics")
+us_plot_zoomed <- ggplot(us_sf, aes(x=Year,y=value, colour=variable)) +
+  geom_line() +
+  scale_y_continuous(limits=c(0,180))+
+  scale_colour_discrete(name="") +
+  theme(legend.position = "left") +
+  labs(x="Year", y="Number of", title="Zoomed in Death/Attack Numbers")
+ggarrange(us_plot, us_plot_zoomed, proparticle, ncol=1)
+  #geom_line(data=us_sf, aes(x=Year, y=Deaths)) +
+  #geom_line(data=us_sf, aes(x=Year, y=Attacks)) +
+  #geom_line(data=sf_prop, aes(x=Year, y=Proportion))
